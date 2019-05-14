1fb1c7882c244648ab922c8175467f369964421e
ihsavru <vurvashi11@gmail.com>
2019-05-14 15:47:39 +0530

    add jobs tab

--- app/urls.py
+++ app/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('dash', views.dash, name='dash'),
+    path('jobs', views.jobs, name='jobs'),
     path('accounts/', include('django.contrib.auth.urls'))
 ]
--- app/views.py
+++ app/views.py
@@ -6,4 +6,7 @@ def index(request):
     return render(request, 'app/home.html')
 
 def dash(request):
-    return render(request, 'app/dash.html')
+    return render(request, 'app/dash.html', { 'active': 'dash'})
+
+def jobs(request):
+    return render(request, 'app/jobs.html', { 'active': 'jobs'})
