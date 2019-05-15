f1bc30a7494e71530164cae87a8abcb587e2da32
ihsavru <vurvashi11@gmail.com>
2019-05-16 00:41:25 +0530

    add more files

--- app/views.py
+++ app/views.py
@@ -11,8 +11,14 @@ def dash(request):
 def jobs(request):
     return render(request, 'app/jobs.html', { 'active': 'jobs'})
 
+def job_desc(request):
+    return render(request, 'app/job_desc.html', { 'active': 'jobs'})
+
 def candidates(request):
     return render(request, 'app/candidates.html', { 'active': 'candidates'})
+
+def candidates_analysis(request):
+    return render(request, 'app/candidates_analysis.html', { 'active': 'candidates'})
 
 def feedbacks(request):
     return render(request, 'app/feedbacks.html', { 'active': 'feedbacks'})
