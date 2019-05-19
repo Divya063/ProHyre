84c20d16d595d7dd26cf766018512758e54dd48c
ihsavru <vurvashi11@gmail.com>
2019-05-20 00:30:51 +0530

    fix styling yet again

--- app/static/app/js/base.js
+++ app/static/app/js/base.js
@@ -1,5 +1,5 @@
 window.onload = () => {
-    let loginButton = document.getElementsByClassName("login-btn")[0];
+    let loginButton = document.getElementsByClassName("signup-btn")[0];
     loginButton.onclick = () => {
         document.getElementsByClassName("popup")[0].classList.toggle("visible");
     }
